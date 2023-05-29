80b8ce5ec9f8d215764d9e4da1b10e63367f8ffa
emon1995 <emonhossain995@gmail.com>
2023-05-29 12:08:17 +0600

    users route added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -8,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.idgt1xz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,6 +24,7 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const usersCollection = client.db("bistroBossDB").collection("users");
     const menuCollection = client.db("bistroBossDB").collection("menu");
     const reviewCollection = client.db("bistroBossDB").collection("reviews");
     const cartCollection = client.db("bistroBossDB").collection("carts");
@@ -33,17 +34,29 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
+    // users related apis
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // menu related apis
     // get menu
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find({}).toArray();
       res.send(result);
     });
 
+    // review related apis
+
     // get reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find({}).toArray();
       res.send(result);
     });
+
+    // carts related apis
 
     //  get cart route
     app.get("/carts", async (req, res) => {
@@ -59,7 +72,7 @@ async function run() {
     // add to cart route
     app.post("/carts", async (req, res) => {
       const item = req.body;
-      console.log(item);
+      // console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
@@ -67,6 +80,7 @@ async function run() {
     // delete cart item
     app.delete("/carts/:id", async (req, res) => {
       const id = req.params.id;
+      // console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
