57dfd4d90064db7e0ea4cba96bd46aca2892f473
emon1995 <emonhossain995@gmail.com>
2023-05-29 19:08:11 +0600

    get admin route added with secure

--- index.js
+++ index.js
@@ -85,6 +85,20 @@ async function run() {
       res.send(result);
     });
 
+    // get admin role route secure with middleware
+    app.get("/users/admin/:email", verifyJWTToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
     // admin user patch route
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
