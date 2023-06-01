c4b215fc40477f51c7dda1ed8155c2295e173bdc
emon1995 <emonhossain995@gmail.com>
2023-06-01 19:15:28 +0600

    delete item route added

--- index.js
+++ index.js
@@ -139,6 +139,14 @@ async function run() {
       res.send(result);
     })
 
+    // item delete api
+    app.delete("/menu/:id", verifyJWTToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // review related apis
 
     // get reviews
